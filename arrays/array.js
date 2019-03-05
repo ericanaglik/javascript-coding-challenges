4d38fe792c3fd4142d061541a879ba07674b8c7f
erica2605@live.com <erica2605@live.com>
2019-03-05 15:02:15 -0800

    2D array challenge working

--- arrays/array.js
+++ arrays/array.js
@@ -1,12 +1,27 @@
+/*************************
+       CHALLENGE 1:
+Make a 2D Array that takes
+width and height as a
+parameter!
+*************************/
+const arr = [];
+
 function make2dArray(width, height) {
     const arr = [];
+    
+    for (let num = 0; num < height; num += 1) {
+        const arr2 = [];
 
-    /// ???
-
-    return arr
+        for (let i = 0; i < width; i += 1) {
+            arr2.push(0);
+        }
+        arr.push(arr2);
+        
+    }
+    return arr;
 }
 
-make2dArray(3, 3)
+console.log(make2dArray(4, 9))
 /*
 
 [
