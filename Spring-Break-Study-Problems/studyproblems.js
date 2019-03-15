83f08eac706ab4d888bcfa9877e4577a81b6ba64
erica2605@live.com <erica2605@live.com>
2019-03-14 17:08:03 -0700

    started practice problems

---
+++ Spring-Break-Study-Problems/studyproblems.js
@@ -0,0 +1,12 @@
+/*****************
+**ARRAY PROBLEMS**
+*****************/
+
+// Make an Array of letters
+
+/* You're going to make a banner that cycles a message.
+You need to start with a string but turn it into an array.
+This will be easier than writing the array from scratch. */
+
+const message = 'Erica Naglik'
+const messageArray = message.split('')
